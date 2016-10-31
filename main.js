65ef0b34730457126f1a2344a4b058a1b14ab111
Shuliang Deng <dsl89125@gmail.com>
2016-10-30 18:40:54 -0700

    change scroll btn

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ $(document).ready(function(){
     });
 
 
+var offset = $(window).scrollTop() - actualBlock.offset().top,
+  windowHeight = $(window).height();
+ 
+if( offset >= -windowHeight && offset <= 0 ) {
+  // section entering the viewport
+  translateY = (-offset)*100/windowHeight;
+    scale = 1;
+  opacity = 1;
+} else if( offset > 0 && offset <= windowHeight ) {
+  //section leaving the viewport 
+    scale = (1 - ( offset * 0.3/windowHeight));
+  opacity = ( 1 - ( offset/windowHeight) );
+  translateY = 0;
+  boxShadowBlur = 40*(offset/windowHeight);
+}
