9f64543e17ca85f03b433f807c6c56f723663d6e
Shuliang Deng <dsl89125@gmail.com>
2016-11-03 21:59:55 -0700

    fix scroll down & change video decription

--- main.js
+++ main.js
@@ -14,18 +14,10 @@ $(document).ready(function(){
     });
 
 
-var offset = $(window).scrollTop() - actualBlock.offset().top,
-  windowHeight = $(window).height();
- 
-if( offset >= -windowHeight && offset <= 0 ) {
-  // section entering the viewport
-  translateY = (-offset)*100/windowHeight;
-    scale = 1;
-  opacity = 1;
-} else if( offset > 0 && offset <= windowHeight ) {
-  //section leaving the viewport 
-    scale = (1 - ( offset * 0.3/windowHeight));
-  opacity = ( 1 - ( offset/windowHeight) );
-  translateY = 0;
-  boxShadowBlur = 40*(offset/windowHeight);
-}
+$(function() {
+  $('#scrollbar').on('click', function(e) {
+    e.preventDefault();
+    // $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    $('body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
